9ebdc8031403c306898e828e9233cea25411772c
Victoria Lyskova <s24787@pjwstk.edu.pl>
2024-03-12 11:28:42 +0100

    Added GetAverage method

--- my_first_app/my_first_app/Program.cs
+++ my_first_app/my_first_app/Program.cs
@@ -4,3 +4,14 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Modyfikacja 1");
 Console.WriteLine("Modyfikacja 2");
 Console.WriteLine("Modyfikacja 3");
+
+static double GetAverage(int[] numbers)
+{
+    int sum = 0;
+    foreach (var num in numbers)
+    {
+        sum += num;
+    }
+
+    return (double) sum / numbers.Length;
+}
